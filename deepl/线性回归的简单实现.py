878c1cf9e2085052f17bd85a2f7dbdcda0440ba9
LDZ6 <124798323+LDZ6@users.noreply.github.com>
2024-08-17 17:51:08 +0800

    线性回归的简单实现

--- deepl/线性回归的简单实现.py
+++ deepl/线性回归的简单实现.py
@@ -1,55 +1,50 @@
+import numpy as np
 import torch
+from torch.utils import data
+from d2l import torch as d2l
 from torch import nn
-import random
 
 # 1. 生成数据集
-def synthetic_data(w, b, num_examples):
-    """生成 y = Xw + b + 噪声"""
-    X = torch.normal(0, 1, (num_examples, len(w)))
-    y = torch.matmul(X, w) + b
-    y += torch.normal(0, 0.01, y.shape)  # 添加噪声
-    return X, y.reshape((-1, 1))
-
 true_w = torch.tensor([2, -3.4])
 true_b = 4.2
-features, labels = synthetic_data(true_w, true_b, 1000)
+features, labels = d2l.synthetic_data(true_w, true_b, 1000)
 
-# 2. 定义模型
+# 2. 数据迭代器
+def load_array(data_arrays, batch_size, is_train=True):
+    """构造⼀个PyTorch数据迭代器"""
+    dataset = data.TensorDataset(*data_arrays)
+    return data.DataLoader(dataset, batch_size, shuffle=is_train)
+
+batch_size = 10
+data_iter = load_array((features, labels), batch_size)
+print(next(iter(data_iter)))
+
+# 3. 定义模型
 net = nn.Sequential(nn.Linear(2, 1))
 
-# 3. 初始化模型参数
+# 4. 初始化模型参数
 net[0].weight.data.normal_(0, 0.01)
 net[0].bias.data.fill_(0)
 
-# 4. 定义损失函数
+# 5. 定义损失函数
 loss = nn.MSELoss()
 
-# 5. 定义优化算法
+# 6. 定义优化算法
 trainer = torch.optim.SGD(net.parameters(), lr=0.03)
 
-# 6. 训练模型
-def data_iter(batch_size, features, labels):
-    num_examples = len(features)
-    indices = list(range(num_examples))
-    random.shuffle(indices)
-    for i in range(0, num_examples, batch_size):
-        batch_indices = torch.tensor(indices[i: min(i + batch_size, num_examples)])
-        yield features[batch_indices], labels[batch_indices]
-
-batch_size = 10
+# 7. 训练模型
 num_epochs = 3
-
 for epoch in range(num_epochs):
-    for X, y in data_iter(batch_size, features, labels):
-        l = loss(net(X), y)
-        trainer.zero_grad()
-        l.backward()
-        trainer.step()
-    l = loss(net(features), labels)
+    for X, y in data_iter:
+        l = loss(net(X), y)  # 计算预测值和真实值之间的损失
+        trainer.zero_grad()  # 梯度清零
+        l.backward()  # 反向传播，计算梯度
+        trainer.step()  # 更新模型参数
+    l = loss(net(features), labels)  # 计算整个数据集上的损失
     print(f'epoch {epoch + 1}, loss {l:f}')
 
-# 7. 输出训练结果
+# 8. 评估模型
 w = net[0].weight.data
-print('w的估计误差：', true_w - w.reshape(true_w.shape))
 b = net[0].bias.data
+print('w的估计误差：', true_w - w.reshape(true_w.shape))
 print('b的估计误差：', true_b - b)
