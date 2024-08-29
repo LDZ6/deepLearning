6f7e9b2e07a6960cbe8170b6591ba7a82bcc95fb
LDZ6 <124798323+LDZ6@users.noreply.github.com>
2024-08-29 12:05:06 +0800

    预测房价

--- deepl/utils.py
+++ deepl/utils.py
@@ -26,8 +26,6 @@ def softmax(X):
     return X_exp / partition
 
 # 定义模型
-def net(X):
-    return softmax(torch.matmul(X.reshape((-1, W.shape[0])), W) + b)
 
 # 定义交叉熵损失函数
 def cross_entropy(y_hat, y):
@@ -149,9 +147,6 @@ def train_ch3(net, train_iter, test_iter, loss, num_epochs, updater):
     plt.show()
 
 
-# 定义优化器
-def updater(batch_size):
-    return d2l.sgd([W, b], lr, batch_size)
 
 def predict_ch3(net, test_iter, n=6):
     """预测标签（定义见第3章）"""
@@ -174,3 +169,63 @@ def predict_ch3(net, test_iter, n=6):
     )
     plt.tight_layout()
     plt.show()  # 在 PyCharm 中显示图像
+
+import hashlib
+import os
+import tarfile
+import zipfile
+import requests
+
+# 数据中心，用于存储数据集的下载链接和对应的sha-1哈希值
+DATA_HUB = dict()
+DATA_URL = 'http://d2l-data.s3-accelerate.amazonaws.com/'
+
+
+def download(name, cache_dir=os.path.join('..', 'data')):
+    """下载一个DATA_HUB中的文件，返回本地文件名"""
+    assert name in DATA_HUB, f"{name} 不存在于 {DATA_HUB}"
+    url, sha1_hash = DATA_HUB[name]
+    os.makedirs(cache_dir, exist_ok=True)
+    fname = os.path.join(cache_dir, url.split('/')[-1])
+
+    # 如果文件存在，且sha1值匹配，则使用缓存文件
+    if os.path.exists(fname):
+        sha1 = hashlib.sha1()
+        with open(fname, 'rb') as f:
+            while True:
+                data = f.read(1048576)
+                if not data:
+                    break
+                sha1.update(data)
+        if sha1.hexdigest() == sha1_hash:
+            return fname  # 命中缓存
+
+    # 下载文件
+    print(f'正在从 {url} 下载 {fname}...')
+    r = requests.get(url, stream=True, verify=True)
+    with open(fname, 'wb') as f:
+        f.write(r.content)
+    return fname
+
+
+def download_extract(name, folder=None):
+    """下载并解压zip/tar文件"""
+    fname = download(name)
+    base_dir = os.path.dirname(fname)
+    data_dir, ext = os.path.splitext(fname)
+
+    if ext == '.zip':
+        fp = zipfile.ZipFile(fname, 'r')
+    elif ext in ('.tar', '.gz'):
+        fp = tarfile.open(fname, 'r')
+    else:
+        assert False, '只有zip/tar文件可以被解压缩'
+
+    fp.extractall(base_dir)
+    return os.path.join(base_dir, folder) if folder else data_dir
+
+
+def download_all():
+    """下载DATA_HUB中的所有文件"""
+    for name in DATA_HUB:
+        download(name)
