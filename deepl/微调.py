48a570091ac6f394fdab4a5a8958a9b0dad7cf50
LDZ6 <124798323+LDZ6@users.noreply.github.com>
2024-09-16 00:21:18 +0800

    微调

---
+++ deepl/微调.py
@@ -0,0 +1,73 @@
+import os
+import torch
+import torchvision
+from torch import nn
+from d2l import torch as d2l
+
+# 下载并解压热狗数据集
+d2l.DATA_HUB['hotdog'] = (d2l.DATA_URL + 'hotdog.zip', 'fba480ffa8aa7e0febbb511d181409f899b9baa5')
+data_dir = d2l.download_extract('hotdog')
+
+# 读取训练和测试数据集
+train_imgs = torchvision.datasets.ImageFolder(os.path.join(data_dir, 'train'))
+test_imgs = torchvision.datasets.ImageFolder(os.path.join(data_dir, 'test'))
+
+# 显示前8个正类样本图片和最后8张负类样本图片
+hotdogs = [train_imgs[i][0] for i in range(8)]
+not_hotdogs = [train_imgs[-i - 1][0] for i in range(8)]
+d2l.show_images(hotdogs + not_hotdogs, 2, 8, scale=1.4)
+
+# 使用RGB通道的均值和标准差，以标准化每个通道
+normalize = torchvision.transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+train_augs = torchvision.transforms.Compose([
+    torchvision.transforms.RandomResizedCrop(224),
+    torchvision.transforms.RandomHorizontalFlip(),
+    torchvision.transforms.ToTensor(),
+    normalize
+])
+test_augs = torchvision.transforms.Compose([
+    torchvision.transforms.Resize([256, 256]),
+    torchvision.transforms.CenterCrop(224),
+    torchvision.transforms.ToTensor(),
+    normalize
+])
+
+# 定义和初始化模型
+pretrained_net = torchvision.models.resnet18(pretrained=True)
+pretrained_net.fc = nn.Linear(pretrained_net.fc.in_features, 2)  # 修改输出层为两类分类任务
+nn.init.xavier_uniform_(pretrained_net.fc.weight)
+
+
+# 定义训练函数
+def train_fine_tuning(net, learning_rate, batch_size=128, num_epochs=5, param_group=True):
+    train_iter = torch.utils.data.DataLoader(
+        torchvision.datasets.ImageFolder(os.path.join(data_dir, 'train'), transform=train_augs),
+        batch_size=batch_size, shuffle=True
+    )
+    test_iter = torch.utils.data.DataLoader(
+        torchvision.datasets.ImageFolder(os.path.join(data_dir, 'test'), transform=test_augs),
+        batch_size=batch_size
+    )
+    devices = d2l.try_all_gpus()
+    loss = nn.CrossEntropyLoss(reduction="none")
+
+    # 根据需要设定不同参数组的学习率
+    if param_group:
+        params_1x = [param for name, param in net.named_parameters() if name not in ["fc.weight", "fc.bias"]]
+        trainer = torch.optim.SGD([{'params': params_1x},
+                                   {'params': net.fc.parameters(), 'lr': learning_rate * 10}],
+                                  lr=learning_rate, weight_decay=0.001)
+    else:
+        trainer = torch.optim.SGD(net.parameters(), lr=learning_rate, weight_decay=0.001)
+
+    # 训练模型
+    d2l.train_ch13(net, train_iter, test_iter, loss, trainer, num_epochs, devices)
+
+
+# 微调预训练模型
+train_fine_tuning(pretrained_net, 5e-5)
+
+# 从零开始训练一个新的模型用于对比
+scratch_net = torchvision.models.resnet18()
+scratch_net.fc = nn.Linear(scratch_net.fc.in_features, 2)
+train_fine_tuning(scratch_net, 5e-4, param_group=False)
